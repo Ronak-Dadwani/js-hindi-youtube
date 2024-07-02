5485cbc7b12c65be023951283965e921e1280822
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-02 12:24:49 +0000

    done with comparisions

---
+++ 01_Basic/04_Comparisions.js
@@ -0,0 +1,27 @@
+// console.log(2>1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// avoid this type of comparision
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0 );
+// console.log(undefined > 0);
+// console.log(undefined < 0 );
+
+// this gets it into trouble and make more confusion while comparing.
+
+
+// === strict check 
+
+// console.log("2 " === 2 );
