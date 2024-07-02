608d536cb726d8a235f42eb9231684f6fb19b995
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-02 12:47:44 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,59 @@
+//  Primitive datatypes
+
+//typeof 
+/*
+undefined = undefined
+null = object
+boolean = bollean
+number = number
+string = string
+object (native and does not implement ) = object
+
+*/
+
+// 7 types : string , Number, boolean , null , undefined , symbol , bigint , 
+
+// const score = 100;
+// const scoreValue = 100.3;
+
+// const isLoggedIn = false;
+// const outsideTemp = null;
+
+let userEmail = undefined; // two ways of writting undefined value 
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+// const bigNumber = 95621325415635;
+// console.log(bigNumber);
+// console.log(typeof bigNumber);
+
+// Is javascript is a static type or dynamically type => this is a dynamically type.
+
+// Reference Types (non-primitive datatypes)
+
+//array , objects , function (=> web evenets and browser events ) 
+
+
+// const heros = ['shaktiman','naagraj', "doga"]
+
+// let myObj = {
+//     name: "ronak",
+//     age: 22 ,
+
+// }
+
+// what all things are their in culry braces all are objects.
+
+
+// function  as a variable
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof anotherId);
+
