63ce81a80aa3d2cd4f98c52950ac542efb870414
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-02 07:01:31 +0000

    convertion of data types

---
+++ 01_Basic/03_ConvertionsOperations.js
@@ -0,0 +1,32 @@
+// let score = "ronak";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//null value 0 
+// undefined value = NaN
+//true = 1
+//false = 0
+//string = NaN
+
+
+// let isLoggedIn = "ronak";
+
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true;
+//0 => false
+// "" => empty string false
+//"ronak" => true
+
+let someNumber = 23;
+
+let stringNumber = String(someNumber);
+console.log( stringNumber);
+console.log(typeof stringNumber);
