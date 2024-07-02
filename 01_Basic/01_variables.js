9336fa5b9ce5da0bcae3f33266e8250c2d874135
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-02 06:31:55 +0000

    DONE WITH DATATYPES

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+
+
+accountCity = "Jaipur";
+
+// accountId = 2 not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "12122112"
+accountCity = 'Bengaluru'
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
+
+/*
+preferred not to use var varibale 
+because of issue in block scope and functional scope
+*/
