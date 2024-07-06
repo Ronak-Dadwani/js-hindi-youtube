706de2926f8484ee20ce319012a5ab096d5c8da0
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-06 12:42:04 +0000

    array_part_2

---
+++ 02_Basic/01_array.js
@@ -0,0 +1,37 @@
+//array
+const myArr = [0,1,2,3,4,5]
+// const myHeros = ["shaktiman", "nagraj"]
+
+// const myArr2 = new Array(1 , 2 , 3 , 4)
+// console.log(Arr[0]);
+
+// Array methods
+
+// Arr.push(6)
+// Arr.push(7)
+// Arr.pop()
+
+// Arr.unshift(9)
+// Arr.shift()
+
+// console.log(Arr.includes(9));
+// console.log(Arr.indexOf(3));
+
+
+
+// console.log(Arr);
+
+
+//slice and splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("C", myArr);
+console.log(myn2);
