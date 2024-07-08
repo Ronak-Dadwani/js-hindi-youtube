ae606be4ef296cd2bc212f503b1e3800d037633a
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-08 05:02:33 +0000

    done with functions.

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -40,4 +40,38 @@ function loginUserMessage (username = "sam")  {
 }
  
 // console.log(loginUserMessage("ronak")) // output will be ronak just logged in
-console.log(loginUserMessage("ronak")); // output undefined just logged in null will not when an empty function is written
+// console.log(loginUserMessage("ronak")); // output undefined just logged in null will not when an empty function is written
+
+function calculateCartPrice(val1 , val2 , ...num1){  // ... is for rest operator
+    return num1
+}
+
+// console.log(calculateCartPrice (200 , 300 , 400,3000));
+
+const user = {
+    username: "ronak",
+    price: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is  ${anyobject.price}`);
+
+}
+
+// handleObject(user)
+
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+
+const myNewArray = [200,300,500,400]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+
+// console.log(returnSecondValue(myNewArray));
+
