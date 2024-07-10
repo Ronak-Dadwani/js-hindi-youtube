3f8ad7bc77f5a8dd8e92480dff398a6e4558fa67
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-10 10:42:53 +0000

    control flow basic

---
+++ 04_Control-Flow/one.js
@@ -0,0 +1,40 @@
+// control flow 
+
+//if
+
+const isUserloggedIn = true
+
+
+
+// if (temparature = 41) {
+//     console.log("less than 50");
+// } else {
+//     console.log("greater than 50");
+// }
+
+
+// const score = 200 
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+const balance = 1000
+// if (balance > 500) console.log("test"); // implicent type for writting in one line if condition
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInfromGoogle = false
+const loggedFromemail = true
+
+if (userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy courses");
+
+if (loggedInfromGoogle || loggedFromemail) {
+    console.log("User logged in");
+}
+}
+
+
