8fd8038e09062a950042b918b92acb4055776b24
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-13 07:39:21 +0000

    loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+// let index = 0
+// while (index <=20) {
+//     console.log(`Value of indx is ${index}`);
+//     index = index +3
+// }
+
+let myArray = ['flash','batman','shaktiman']
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr +1
+// }
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score <=10);
