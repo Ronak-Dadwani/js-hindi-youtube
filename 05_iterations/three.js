acbb8318fc6600950bbbbda600a1691a233e464a
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-13 11:58:04 +0000

    object and loops

---
+++ 05_iterations/three.js
@@ -0,0 +1,35 @@
+const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greeting = "Hello World!"
+// for (const greet of greeting) {
+//     console.log(greet);   
+// }
+
+// maps
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States of America")
+map.set('Fr',"Freance")
+map.set('IN',"India")
+
+// console.log(map);
+
+// for (const [key , Value] of map) {
+//     console.log(key,' :- ', Value);
+// }
+
+// const myObject = {
+//     'game' : 'NFS',
+//     'game': 'spiderman'
+// }
+
+// for (const [key , value] of myObject) {
+//     console.log(key , ':-', value);
+// }
+
+
