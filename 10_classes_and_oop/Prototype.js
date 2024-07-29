b466af5c60f4e8edb6b33fe54b37abdf91ade547
Ronak Dadwani <116190191+Ronak-Dadwani@users.noreply.github.com>
2024-07-29 18:59:10 +0000

    prototype part 1

---
+++ 10_classes_and_oop/Prototype.js
@@ -0,0 +1,20 @@
+// let myName = "ronak     "
+// let myChannel = "chaiiiiii     "
+
+// console.log(myName.trueLength);
+
+let myHeros = ["thor", "spiderman"]
+
+let herosPower = {
+    thor: "hammer",
+    spiderman : "sling",
+
+
+    getSpiderPower: function(){
+        console.log(`spidy power is ${this.spiderman}`);
+    }
+}
+
+
+
+herosPower.ronak()
